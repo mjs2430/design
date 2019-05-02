41c8ddb0444e026e05a88fd6499caea181014607
Jay Pilgreen <jpilgreen@mcclatchy.com>
2019-05-02 08:50:20 -0500

    Publishing to gh-pages

---
+++ js/jay-walkups.js
@@ -0,0 +1,33 @@
+class JayWalkups extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  get songs() {
+    return [
+      `"This is a Game" by Nick Waterhouse`,
+      `"Unsquare Dance" by Dave Brubeck`,
+      `"This is America" by Childish Gambino`,
+      `"Calabria 2008" by Enur`,
+      `"Boom Boom" by John Lee Hooker`,
+      `"Sinnerman" by Nina Simone`,
+      `"Bamboleo" by the Gipsy Kings`
+    ]
+  }
+
+  connectedCallback() {
+    let observer = new IntersectionObserver(this.swap.bind(this));
+    observer.observe(this);
+  }
+
+  swap(entries) {
+    let e = entries[0];
+
+    if(e.isIntersecting) {
+      let r = Math.floor(Math.random() * this.songs.length);
+      this.textContent = this.songs[r];
+    }
+  }
+}
+
+customElements.define("jay-walkups", JayWalkups);
