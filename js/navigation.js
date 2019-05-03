3c304ab977ad4970d06596494656e4e33eaef357
Jay Pilgreen <jpilgreen@mcclatchy.com>
2019-05-03 15:13:13 -0500

    Publishing to gh-pages

---
+++ js/navigation.js
@@ -0,0 +1,13 @@
+/**
+ * Sample placeholder
+ */
+
+function showNavigation() {
+  alert("Menu coming in a later release");
+}
+
+function toggleSearchForm() {
+  document.querySelectorAll(".flag").forEach(d => {
+    d.classList.toggle("open");
+  });
+}
