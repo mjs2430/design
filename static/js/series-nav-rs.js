a42902a605a8be7ab699b243aacb32715fb92e07
Jay Pilgreen <jpilgreen@mcclatchy.com>
2019-05-16 09:55:12 -0500

    related stories test to show

--- static/js/series-nav-rs.js
+++ static/js/series-nav-rs.js
@@ -10,53 +10,107 @@ class SeriesNav extends HTMLElement {
   get template() {
     let t = document.createElement("template");
     t.innerHTML = `
-      <link rel="stylesheet" href="/design/css/atoms.css">
-      <link rel="stylesheet" href="/design/css/molecules.css">
-      <link rel="stylesheet" href="/design/css/cards.css">
-
       <style>
         :host {
-          --tc: white;
-          --lc: white;
           max-width: 100% !important;
           background-color: #373737;
           display: block !important;
           padding: 15px 0 !important;
+          color: white !important;
         }
 
-        .center {
-          text-align: center;
+        h1 {
+          font: 400 30px/1.2em "McClatchy Serif", serif;
+        }
+
+        h3 {
+          font: 400 18px/1.2em "McClatchy Serif", serif;
+        }
+
+        h5 {
+          font: 600 14px/1.2em "McClatchy Sans", sans-serif;
+        }
+
+        time {
+          display: block;
+          font: 300 11px/1.2em "McClatchy Sans", sans-serif;
+        }
+
+        article {
+          position: relative;
+        }
+
+        figure {
+          margin: 0;
+        }
+
+        figure img {
+          display: block;
+          width: 100%;
+        }
+
+        .top {
           display: block;
           max-width: 720px;
           margin: 0 auto;
         }
 
-        h1 {
-          font-weight: 400;
+        .summary {
+          font: 400 14px/1.5em "McClatchy Sans", sans-serif;
+        }
+
+        .package {
+          position: relative;
+          padding: 15px;
+          text-align: center;
+        }
+
+        .package > * {
+          margin: 10px 0 0 0;
+        }
+
+        .package > :first-child {
+          margin-top: 0;
         }
 
         .grid {
+          display: grid;
           grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+          grid-gap: 30px;
           max-width: 1140px;
           margin: 30px auto;
         }
 
-        .grid .package {
-          display: block;
-          text-align: center;
-        }
-
-        .grid time {
-          display: block;
+        .overlay {
+          position: absolute;
+          bottom: 0;
+          width: 100%;
+          box-sizing: border-box;
+          padding: 45px 30px 15px;
+          color: white;
+          background: linear-gradient(transparent, rgba(0,0,0,0.4) 30%, rgba(0,0,0,0.8));
         }
       </style>
 
-      <div class="package center">
+      <div class="package top">
         <h5>MORE FROM THIS SERIES</h5>
         <h1>${this.title}</h1>
         <p class="summary">${this.summary}</p>
       </div>
-      <div class="grid"></div>
+      <div class="grid">
+        ${ this.feed.map((story, i) => `
+        <article>
+          <figure>
+            <img src="${story.lede}">
+          </figure>
+          <div class="package overlay">
+            <h5>PART ${i + 1}</h5>
+            <h3>${ story.title }</h3>
+            <time>${ story.time }</time>
+          </div>
+        </article>
+        `.trim()).join("")}
+      </div>
     `;
     return t;
   }
@@ -65,31 +119,73 @@ class SeriesNav extends HTMLElement {
     super();
   }
 
-  connectedCallback() {
-    this.cardTemplate = document.createElement("template");
+  async connectedCallback() {
     let rs = document.querySelector(".related-stories");
 
     if(rs === null) {
       return;
     }
 
+    let articles = Array.from(rs.querySelectorAll("article"));
+    this.feed = await this.fetchAll(articles);
+
     rs.classList.add("related-series");
     rs.attachShadow({ mode: "open" });
     rs.shadowRoot.appendChild(this.template.content.cloneNode(true));
+  }
 
-    this.grid = rs.shadowRoot.querySelector(".grid");
+  /**
+   * Returns a Promise of all related story articles  
+   */
 
-    rs.querySelectorAll("article").forEach((story, i) => {
-      let a = document.createElement("article");
-      a.innerHTML = `
-        <div class="package">
-          <h5>PART ${i + 1}</h5>
-          <h3>${ story.querySelector("h3").innerText }</h3>
-          <time>${ story.querySelector("time").innerText }</time>
-        </div>
-      `;
-      this.grid.appendChild(a);
-    });
+  async fetchAll([...articles]) {
+    return Promise.all(
+      articles.map(article => { 
+        return this.fetch(article) 
+      })
+    );
+  }
+
+  /**
+   * Fetches an article by url and returns metadata
+   */
+
+  async fetch(article) {
+    let item = {
+      url: article.querySelector("h3 a").href,
+      title: article.querySelector("h3").innerText.trim(),
+      time: article.querySelector("time").innerText.trim()
+    }
+
+    let response = await fetch(item.url);
+    let text = await response.text();
+    let t = document.createElement('template');
+    t.innerHTML = text;
+
+    item.lede = t.content.querySelector('[property="og:image"]').getAttribute('content');
+    item.author = this.getAuthor(text);
+
+    return item;
+  }
+
+  /**
+   * Pulls authors out of the page HTML with regex
+   */
+
+  getAuthor(html) {
+    let contentSource = html.match(/mistats.contentsource="([\w\s]*).*"/)
+
+    if(contentSource.length > 1) {
+      let authors = contentSource[1].split(",");
+
+      if( authors.length == 1) {
+        return `By ${authors}`;
+      }
+
+      return `By ${ authors.slice(0, -1).join(", ") } and ${ authors.slice(-1) }`;
+    }
+
+    return "";
   }
 }
 
