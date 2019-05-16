4b09e31b7f3732ea7a2007d938e27a670e3cca6a
Jay Pilgreen <jpilgreen@mcclatchy.com>
2019-05-16 11:16:21 -0500

    series nav rs version read next card

--- static/js/series-nav-rs.js
+++ static/js/series-nav-rs.js
@@ -7,16 +7,40 @@ class SeriesNav extends HTMLElement {
     return this.getAttribute("summary");
   }
 
+  get part() {
+    return parseInt(this.getAttribute("part"));
+  }
+
+  get nextCardLocation() {
+    return this.getAttribute("next-card-location");
+  }
+
+  get nextStory() {
+    if( this.part >= this.feed.length ) {
+      return false;
+    }
+
+    return this.feed[this.part]
+  }
+
   get template() {
     let t = document.createElement("template");
     t.innerHTML = `
       <style>
         :host {
           max-width: 100% !important;
-          background-color: #373737;
+          background: var(--background, #373737);
           display: block !important;
           padding: 15px 0 !important;
-          color: white !important;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        a:hover {
+          text-decoration: underline;
         }
 
         h1 {
@@ -28,7 +52,7 @@ class SeriesNav extends HTMLElement {
         }
 
         h5 {
-          font: 600 14px/1.2em "McClatchy Sans", sans-serif;
+          font: 500 14px/1.2em "McClatchy Sans", sans-serif;
         }
 
         time {
@@ -49,10 +73,29 @@ class SeriesNav extends HTMLElement {
           width: 100%;
         }
 
+        .label {
+          margin: 0;
+          display: flex;
+          justify-content: center;
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          z-index: 2;
+        }
+
+        .label > * {
+          margin: 0;
+          padding: 5px 10px 3px;
+          color: white;
+          background-color: #373737;
+        }
+
         .top {
           display: block;
           max-width: 720px;
           margin: 0 auto;
+          color: var(--tc, white);
         }
 
         .summary {
@@ -75,10 +118,17 @@ class SeriesNav extends HTMLElement {
 
         .grid {
           display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
           grid-gap: 30px;
           max-width: 1140px;
-          margin: 30px auto;
+          margin: 0 auto;
+          padding: 15px 0;
+        }
+
+        @media(min-width: 768px) {
+          .grid {
+            grid-template-columns: 1fr 1fr;
+            padding: 15px;
+          }
         }
 
         .overlay {
@@ -87,8 +137,8 @@ class SeriesNav extends HTMLElement {
           width: 100%;
           box-sizing: border-box;
           padding: 45px 30px 15px;
+          background: linear-gradient(transparent, rgba(0,0,0,0.6) 45px, rgba(0,0,0,0.9));
           color: white;
-          background: linear-gradient(transparent, rgba(0,0,0,0.4) 30%, rgba(0,0,0,0.8));
         }
       </style>
 
@@ -100,12 +150,17 @@ class SeriesNav extends HTMLElement {
       <div class="grid">
         ${ this.feed.map((story, i) => `
         <article>
+          ${i == this.part ? `
+          <div class="label sticky">
+            <h5 class="promo">READ NEXT</h5>
+          </div>
+          ` : ``}
           <figure>
             <img src="${story.lede}">
           </figure>
           <div class="package overlay">
             <h5>PART ${i + 1}</h5>
-            <h3>${ story.title }</h3>
+            <h3><a href="${story.url}">${ story.title }</a></h3>
             <time>${ story.time }</time>
           </div>
         </article>
@@ -132,6 +187,13 @@ class SeriesNav extends HTMLElement {
     rs.classList.add("related-series");
     rs.attachShadow({ mode: "open" });
     rs.shadowRoot.appendChild(this.template.content.cloneNode(true));
+
+    if(this.nextCardLocation) {
+      let ip = document.querySelector(this.nextCardLocation);
+      if(ip) {
+        ip.after(this.readNext.content.cloneNode(true));
+      }
+    }
   }
 
   /**
@@ -186,6 +248,40 @@ class SeriesNav extends HTMLElement {
     }
 
     return "";
+  }
+
+  /**
+   * Next horizontal card
+   */
+
+  get readNext() {
+    let t = document.createElement("template");
+    t.innerHTML = `
+      <style>
+        .sn-readnext {
+          padding: 0;
+          min-height: 200px;
+          flex-wrap: wrap;
+        }
+
+        .sn-readnext .package {
+          flex-grow: 3;
+        }
+      </style>
+
+      <div class="card horizontal impact sn-readnext">
+        <figure>
+          <div class="label sticky"><h5 class="impact">READ NEXT</h5></div>
+          <a href="${this.nextStory.url}"><img src="${this.nextStory.lede}"></a>
+        </figure>
+        <div class="package">
+          <h5>PART ${this.part + 1} OF ${this.feed.length} in ${this.title.toUpperCase()}</h5>
+          <h3><a href="${this.nextStory.url}">${this.nextStory.title}</a></h3>
+          <h6>${this.nextStory.author.toUpperCase()}</h6>
+        </div>
+      </div>
+    `;
+    return t;
   }
 }
 
