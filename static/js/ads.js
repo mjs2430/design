892151f6b6c97ec60b93e4c690b1d67eb7e76190
Jay Pilgreen <jpilgreen@mcclatchy.com>
2019-07-18 11:09:19 -0500

    fixes for ads causing story-body to go wide

--- static/js/ads.js
+++ static/js/ads.js
@@ -16,7 +16,13 @@ class Ads {
 
     googletag.cmd.push(() => {
       this.sizeMaps = {
-        "leaderboard": googletag.sizeMapping().addSize([1024, 600], [[970, 250],[728, 90]]).addSize([750, 400], [728, 90]).addSize([0, 0], [320, 50]).build()
+        "leaderboard": googletag.sizeMapping()
+          .addSize([1024, 600], [[728, 90],[970, 250]])
+          .addSize([750, 400], [728, 90])
+          .addSize([0, 0], [320, 50]).build(),
+        "flex": googletag.sizeMapping()
+          .addSize([768, 600], [300, 600])
+          .addSize([0,0], [300, 250]).build()
       }
 
       googletag.pubads().enableSingleRequest();
