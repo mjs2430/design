19ad65862ef976d8a589c75bee99e0ac1b24097a
Jay Pilgreen <jpilgreen@mcclatchy.com>
2019-08-09 13:42:28 -0500

    impact2020 first draft

--- content/saratoga/editor/app/app.js
+++
@@ -1,9 +0,0 @@
-import {uid, debounce} from './utils.js';
-
-var editor = new Mobiledoc.Editor({
-  placeholder: "Start writing here ..",
-  autofocus: true
-})
-
-var element = document.querySelector("#editor");
-editor.render(element);
